bd1e31f2f3b3eaccb0dd722dfe99eb88229f114e
Juan Cabrera <juan.acc@gmail.com>
2015-02-03 17:13:15 -0500

    first commit — core

---
+++ README.md
@@ -0,0 +1,12 @@
+Gulp frontend codebase
+======================
+
+A basic gulp codebase project using:
+
+- [Browserify](http://browserify.org/) with [6to5ify](https://6to5.org/docs/using-6to5/#browserify)
+- [Slim](http://slim-lang.com/)
+- [SASS](http://sass-lang.com/)
+- [Autoprefixer](https://github.com/postcss/autoprefixer-core)
+
+### Next steps
+Try and incorporate performance tools (like [uncss](https://github.com/ben-eb/gulp-uncss), [critical-path](https://github.com/addyosmani/critical-path-css-demo)) and testing tools.
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var
+  fs                 = require("fs"), 
+  gulp               = require('gulp'),
+  connect            = require('gulp-connect'),
+  browserify         = require("browserify"), 
+  to5ify             = require("6to5ify"), 
+  sass               = require('gulp-ruby-sass'), 
+  prefix             = require('gulp-autoprefixer'),
+  slim               = require("gulp-slim")
+;
+
+// JavaScript
+gulp.task('to5ify', function() {
+  browserify('./source/js/main.js', { debug: false })
+    .transform(to5ify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(fs.createWriteStream("./dist/js/main.js"));
+});
+
+// Styles
+gulp.task('scss2css', function() {
+  sass('./source/scss/main.scss') 
+  .on('error', function (err) {
+    console.error('Error!', err.message);
+  })
+  .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+  .pipe(gulp.dest('./dist/css/'));
+});
+
+// Templates
+gulp.task('slim2html', function(){
+  gulp.src("./source/slim/*.slim")
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest("./dist/"));
+});
+
+// Local webserver
+gulp.task('connect', function() {
+  connect.server();
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./source/js/*.js', ['to5ify']);
+  gulp.watch('./source/scss/*.scss', ['scss2css']);
+  gulp.watch('./source/slim/*.slim', ['slim2html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
